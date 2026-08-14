ae46e5a03a4d21ff11d64910f104a4b0068356a2
ElvieBlooms <Elvieblooms@pm.me>
2026-08-13 22:57:54 -0400

    Add files via upload

--- main.lua
+++ main.lua
@@ -1,23 +1,35 @@
 return function(mod)
-  local soundPath = mod.assets:path("assets/event.ogg")
-  local duckUntil = 0
+    math.randomseed(os.time())
 
-  local function playSound()
-    local ok, src = pcall(love.audio.newSource, soundPath, "static")
-    if ok and src then
-      duckUntil = love.timer.getTime() + 2.5
+    local newGamePath  = mod.assets:path("assets/new_game.ogg")
+    -- local continuePath = mod.assets:path("assets/continue.ogg")
+    local duckUntil = 0
+
+    local function playSound(path)
+    local ok, src = pcall(love.audio.newSource, path, "static")
+      if ok and src then
+      duckUntil = love.timer.getTime() + 2
       src:play()
+      end
     end
-  end
 
-  mod.hooks:wrap("music.volume", function(next, vol, ctx)
-    vol = next(vol, ctx)
-    if love.timer.getTime() < duckUntil then
+    mod.hooks:wrap("music.volume", function(next, vol, ctx)
+      vol = next(vol, ctx)
+      if love.timer.getTime() < duckUntil then
       return vol * 0.3
-    end
+      end
     return vol
-  end)
+    end)
 
-  mod.events:once("intro.oak_speech.finished", playSound)
-  mod.events:once("save.loaded", playSound)
-end
+    local voicePool = {
+         mod.assets:path("assets/continue1.ogg"),
+         mod.assets:path("assets/continue2.ogg"),
+         mod.assets:path("assets/continue3.ogg"),
+       }
+       local function playRandom(pool)
+         playSound(pool[math.random(#pool)])
+       end
+
+    mod.events:once("intro.oak_speech.finished", function() playSound(newGamePath) end)
+    mod.events:once("save.loaded", function() playRandom(voicePool) end)
+  end
